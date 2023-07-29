280238dff3b059453a37a78f2d7aa2fa4b1ce42f
Sasipha <SasiphaThonkhan11@gmail.com>
2023-07-29 16:58:54 +0700

    6.8

---
+++ Sasipha/src/views/EventListView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import EventCard from '../components/EventCard.vue'
+import type { EventItem } from '@/type'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import axios from 'axios'
+ 
+const events: Ref<Array<EventItem>> = ref([])
+  axios.get<EventItem[]>('http://localhost:3004/events')
+  .then((response) =>{
+    events.value = response.data
+  })
+  
+</script>
+
+<template>
+  <main class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+  </main>
+</template>
+
+<style scoped>
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}</style>
